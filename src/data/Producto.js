a5c16adfa1acd0dc121f726f00349045c0ce0801
barc05 <benjaminarucutipa@gmail.com>
2025-10-08 14:26:43 -0300

    Add files via upload

---
+++ src/data/Producto.js
@@ -0,0 +1,26 @@
+const products = [
+ {
+   id: 1,
+   name: 'Producto 1',
+   description: 'Descripción breve del producto 1.',
+   price: 10000,
+   image: 'https://frutonchile.cl/wp-content/uploads/2019/12/lechuga-costina.jpg' // Imagen placeholder
+ },
+ {
+   id: 2,
+   name: 'Producto 2',
+   description: 'Descripción breve del producto 2.',
+   price: 10000,
+   image: 'https://res.cloudinary.com/freshis/image/upload/v1629562715/Tomate_rama2_aa1fb01ca1.png'
+ },
+ {
+   id: 3,
+   name: 'Producto 3',
+   description: 'Descripción breve del producto 3.',
+   price: 10000,
+   image: 'https://static.libertyprim.com/files/familles/carotte-large.jpg?1569271734'
+ }
+];
+
+
+export default products;
