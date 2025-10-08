ee6946560d42fb997e8d9ee724b8d5e3c471fcda
IgnacioSaavedraDel <ig.saavedrad@duocuc.cl>
2025-10-08 14:34:55 -0300

    tercero

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Container } from 'react-bootstrap';
+
+
+
+function Home() {
+ return (
+   <Container className="my-5">
+     <h1>Plantita</h1>
+     <p>Quienes somos?</p>
+   </Container>
+   
+ );
+}
+
+export default Home;
--- src/pages/Producto.jsx
+++ src/pages/Producto.jsx
@@ -1,74 +1,20 @@
-import React from "react";
-import "../styles/Producto.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Container,Row } from 'react-bootstrap';
+import products from '../data/Producto';
+import ProductCard from '../components/organisms/ProductCard';
 
-function Productos() {
-  return (
-    <div className="cuerpo">
-      <header>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="../index.html">Verde en Casa🌿</a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavAltMarkup"
-              aria-controls="navbarNavAltMarkup"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
+function Producto() {
+ return (
+   <Container className="my-5">
+     <h1>Productos</h1>
+     <Row>
+       {products.map((product) => (
+         <ProductCard key={product.id} product={product} />
+       ))}
+     </Row>
+   </Container>
 
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-                <a className="nav-link active" href="../index.html">Inicio</a>
-                <a className="nav-link active" href="../html/contacto.html">Contacto</a>
-                <a className="nav-link active" href="../">Productos</a>
-                <a className="nav-link active" href="../html/blog.html">Blog</a>
-              </div>
+ );
+}
 
-              <div className="d-flex align-items-center ms-auto">
-                <a className="btn btn-outline-success" href="../html/Carrito.html">
-                  Carrito (<span id="carrito-count">0</span>)
-                </a>
-                <a className="btn btn-outline-dark" href="../html/login.html">Login</a>
-                <a className="btn btn-outline-dark" href="../html/register.html">Registrarse</a>
-              </div>
-            </div>
-          </div>
-        </nav>
-      </header>
 
-      <div className="bloque">
-        <img className="img-ver align-items-center ms-auto" src="../assets/img/verduras.jpg" alt="ver" />
-      </div>
-
-      <div className="contenedor">
-        <div className="caja">
-          <img className="imagen1" src="../assets/img/lechuga-rom.jpg" alt="img1" />
-          <p>Lechuga romana</p>
-          <button type="button" className="btn btn-secondary">Agregar</button>
-          <button type="button" className="btn btn-secondary">Detalles</button>
-        </div>
-
-        <div className="caja">
-          <img className="imagen2" src="../assets/img/Tomate.jpg" alt="img2" />
-          <p>Tomate</p>
-          <button type="button" className="btn btn-secondary">Agregar</button>
-          <button type="button" className="btn btn-secondary">Detalles</button>
-        </div>
-
-        <div className="caja">
-          <img className="imagen2" src="../assets/img/zanahoria.jpg" alt="img2" />
-          <p>Zanahoria</p>
-          <button type="button" className="btn btn-secondary">Agregar</button>
-          <button type="button" className="btn btn-secondary">Detalles</button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default Productos;
+export default Producto;
